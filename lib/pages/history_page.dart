6529d95167e12b8b24bd8a09beab306d577f5cd6
KirillPC <kirya2010777@gmail.com>
2025-07-01 19:46:20 +0300

    CHANGE history list revert

--- lib/pages/history_page.dart
+++ lib/pages/history_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:game_score_counter/data/history_repo.dart';
+import 'package:game_score_counter/data/models/history_model.dart';
 import 'package:game_score_counter/res/app_res.dart';
 import 'package:game_score_counter/widgets/settings_app_bar.dart';
 import 'package:intl/intl.dart';
@@ -15,7 +16,6 @@ class HistoryPage extends StatelessWidget {
       vertical: 12,
     );
 
-
     return Material(
       child: Container(
         color: AppColors.background,
@@ -29,88 +29,105 @@ class HistoryPage extends StatelessWidget {
           children: [
             SettingsAppBar(title: AppTexts.history, hasActions: false),
             Expanded(
-                child: FutureBuilder(
-              future: HistoryRepo().getAll(),
-              builder: (context, snap) {
-                return snap.data == null
-                    ? const CupertinoActivityIndicator()
-                    : ListView.builder(
-                        itemCount: snap.data?.length,
-                        itemBuilder: (BuildContext context, int index) {
-                          final formatter = DateFormat('dd MMM yyyy HH:mm');
-                          String formattedDate = formatter.format(snap.data![index].dateTime).toUpperCase();
+              child: FutureBuilder(
+                future: HistoryRepo().getAll(),
+                builder: (context, snapp) {
 
-                          return Dismissible(
-                            background: Container(
-                              alignment: Alignment.centerRight,
-                              padding: EdgeInsets.all(16),
-                              child: Column(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  Icon(CupertinoIcons.trash_fill, color: AppColors.background,size: 32,),
-                                  Text('Delete', style: AppTypo.body1.copyWith(color: AppColors.background))
-                                ],
-                              ),
+                  /// Give us List<HistoryModel>
+                  List<HistoryModel> invertedList = [];
+                  if (snapp.data != null) {
+                    invertedList.addAll(snapp.data!.reversed);
+                  }
 
-                              decoration: AppDeco.historyDeletingItemDeco,),
-                            key: Key(snap.data![index].dateTime.toString()),
-                            direction: DismissDirection.endToStart,
-                            onDismissed: (direction) {
-                              HistoryRepo().delete(snap.data![index]);
-                            },
-                            child: Container(
-                              clipBehavior: Clip.hardEdge,
-                              margin: const EdgeInsets.only(bottom: 10),
-                              decoration: AppDeco.historyOuterItemDeco,
-                              child: Column(
-                                children: [
-                                  /// Item title
-                                  Padding(
-                                    padding: paddings,
-                                    child: Text(
-                                      formattedDate,
-                                      style: AppTypo.caption2,
+                  return snapp.data == null
+                      ? const CupertinoActivityIndicator()
+                      : ListView.builder(
+                          itemCount: invertedList.length,
+                          itemBuilder: (BuildContext context, int index) {
+                            final formatter = DateFormat('dd MMM yyyy HH:mm');
+                            String formattedDate = formatter
+                                .format(invertedList[index].dateTime)
+                                .toUpperCase();
+
+                            /// uses only one HistoryModel from List
+                            return Dismissible(
+                              background: Container(
+                                alignment: Alignment.centerRight,
+                                padding: const EdgeInsets.all(16),
+                                decoration: AppDeco.historyDeletingItemDeco,
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    const Icon(
+                                      CupertinoIcons.trash_fill,
+                                      color: AppColors.background,
+                                      size: 32,
                                     ),
-                                  ),
-
-                                  /// Item score area
-                                  Container(
-                                    width: double.infinity,
-                                    alignment: Alignment.center,
-                                    decoration: AppDeco.historyInnerItemDeco,
-                                    child: Padding(
+                                    Text('Delete',
+                                        style: AppTypo.body1.copyWith(
+                                            color: AppColors.background))
+                                  ],
+                                ),
+                              ),
+                              key: Key(invertedList[index].dateTime.toString()),
+                              direction: DismissDirection.endToStart,
+                              onDismissed: (direction) {
+                                HistoryRepo().delete(invertedList[index]);
+                              },
+                              child: Container(
+                                clipBehavior: Clip.hardEdge,
+                                margin: const EdgeInsets.only(bottom: 10),
+                                decoration: AppDeco.historyOuterItemDeco,
+                                child: Column(
+                                  children: [
+                                    /// Item title
+                                    Padding(
                                       padding: paddings,
-                                      child: Row(
-                                        children: [
-                                          Expanded(
-                                            child: _buildTeamName(
-                                              snap.data![index].teamName1,
-                                            ),
-                                          ),
-                                          const SizedBox(width: 10),
-                                          Text(
-                                            '${snap.data?[index].teamScore1} - ${snap.data?[index].teamScore2}',
-                                            style: AppTypo.headerM.copyWith(
-                                                color: AppColors.primary1),
-                                          ),
-                                          const SizedBox(width: 10),
-                                          Expanded(
-                                            child: _buildTeamName(
-                                              snap.data![index].teamName2,
-                                            ),
-                                          ),
-                                        ],
+                                      child: Text(
+                                        formattedDate,
+                                        style: AppTypo.caption2,
                                       ),
                                     ),
-                                  )
-                                ],
+
+                                    /// Item score area
+                                    Container(
+                                      width: double.infinity,
+                                      alignment: Alignment.center,
+                                      decoration: AppDeco.historyInnerItemDeco,
+                                      child: Padding(
+                                        padding: paddings,
+                                        child: Row(
+                                          children: [
+                                            Expanded(
+                                              child: _buildTeamName(
+                                                invertedList[index].teamName1,
+                                              ),
+                                            ),
+                                            const SizedBox(width: 10),
+                                            Text(
+                                              '${invertedList[index].teamScore1} - ${invertedList[index].teamScore2}',
+                                              style: AppTypo.headerM.copyWith(
+                                                  color: AppColors.primary1),
+                                            ),
+                                            const SizedBox(width: 10),
+                                            Expanded(
+                                              child: _buildTeamName(
+                                                invertedList[index].teamName2,
+                                              ),
+                                            ),
+                                          ],
+                                        ),
+                                      ),
+                                    )
+                                  ],
+                                ),
                               ),
-                            ),
-                          );
-                        },
-                      );
-              },
-            ))
+                            );
+                          },
+                        );
+                },
+              ),
+            ),
           ],
         ),
       ),
